54ffa531b0b935f890b88fce0e896a1dfb0be99c
Ramon <170786939+r-alejo-z95@users.noreply.github.com>
2024-08-10 22:13:46 -0500

    Add playRound function with winner if...else logic

--- script.js
+++ script.js
@@ -22,6 +22,32 @@ function getHumanChoice() {
     } else {
         return "invalid answer";
     }
-
 }
 
+let humanScore = 0;
+let computerScore = 0;
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+function playRound(humanSelection, computerSelection) {
+    if (humanSelection === "rock" && computerSelection === "rock") {
+        return "Draw!";
+    } else if (humanSelection === "rock" && computerSelection === "paper") {
+        return "Computer wins!";
+    } else if (humanSelection === "rock" && computerSelection === "scissors") {
+        return "Player wins!";
+    } else if (humanSelection === "paper" && computerSelection === "rock") {
+        return "Player wins!";
+    } else if (humanSelection === "paper" && computerSelection === "paper") {
+        return "Draw!";
+    } else if (humanSelection === "paper" && computerSelection === "scissors") {
+        return "Computer wins!";
+    } else if (humanSelection === "scissors" && computerSelection === "rock") {
+        return "Computer wins!";
+    } else if (humanSelection === "scissors" && computerSelection === "paper") {
+        return "Player wins!";
+    } else if (humanSelection === "scissors" && computerSelection === "scissors") {
+        return "Draw!";
+  }
+}
+    
