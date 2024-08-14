74739c5482ae3fed598cd2fea64c6a40d1325d07
Ramon <170786939+r-alejo-z95@users.noreply.github.com>
2024-08-13 23:42:07 -0500

    Simplify functions, insert playRound into playGame Finish overall logic of the game

--- script.js
+++ script.js
@@ -22,15 +22,12 @@ function getHumanChoice() {
         if (playerChoice === "rock") {
             console.log("You have chosen ROCK");
             return "rock";
-            break;
         } else if (playerChoice === "paper") {
             console.log("You have chosen PAPER");
             return "paper";
-            break;
         } else if (playerChoice === "scissors") {
             console.log("You have chosen SCISSORS");
             return "scissors";
-            break;
         } else {
             console.log("That's NOT an option. Try again");
             console.log("----------");
@@ -38,50 +35,40 @@ function getHumanChoice() {
     }
 }
 
-function playRound() {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    if (humanSelection === "rock" && computerSelection === "rock") {
-        console.log("It's a DRAW!");
+    function playRound() {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+    
+        if (humanSelection === "rock" && computerSelection === "scissors" ||
+            humanSelection === "paper" && computerSelection === "rock" ||
+            humanSelection === "scissors" && computerSelection === "paper") {
+            console.log("PLAYER wins this round!");
+            humanScore++;
+        } else if (humanSelection === computerSelection) {
+            console.log("It's a DRAW!");
+        } else {
+            console.log("COMPUTER wins this round!");
+            computerScore++;
+        }
+        console.log(`Score: Player - ${humanScore}, Computer - ${computerScore}`);
         console.log("----------");
-    } else if (humanSelection === "rock" && computerSelection === "paper") {
-        console.log("COMPUTER wins!");
-        console.log("----------");
-    } else if (humanSelection === "rock" && computerSelection === "scissors") {
-        console.log("PLAYER wins!");
-        console.log("----------");
-    } else if (humanSelection === "paper" && computerSelection === "rock") {
-        console.log("PLAYER wins!");
-        console.log("----------");
-    } else if (humanSelection === "paper" && computerSelection === "paper") {
-        console.log("It's a DRAW!");
-        console.log("----------");
-    } else if (humanSelection === "paper" && computerSelection === "scissors") {
-        console.log("COMPUTER wins!");
-        console.log("----------");
-    } else if (humanSelection === "scissors" && computerSelection === "rock") {
-        console.log("COMPUTER wins!");
-        console.log("----------");
-    } else if (humanSelection === "scissors" && computerSelection === "paper") {
-        console.log("PLAYER wins!");
-        console.log("----------");
-    } else {
-        console.log("It's a DRAW!");
-        console.log("----------");
-  }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+
+    console.log("GAME OVER");
+    alert(`Final Score: Player - ${humanScore}, Computer - ${computerScore}`);
 }
-
-
-
-let humanScore = 0;
-let computerScore = 0;
 
 console.log("Let's begin!");
 console.log("----------");
 
 setTimeout(function() {
-    playRound();
-}, 3000);
-
-    
+    playGame();
+}, 1000);
