7965daf1679f158922db782e3e8b53d4803f53eb
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2024-09-26 00:45:16 -0500

    Get rid of playRound function. Add content to the displays

--- script.js
+++ script.js
@@ -3,30 +3,31 @@ const computerDisplay = document.querySelector("#computerDisplay");
 const roundDisplay = document.querySelector("#roundDisplay");
 const gameDisplay = document.querySelector("#gameDisplay");
 const choices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice) {
-    let humanScore = 0;
-    let computerScore = 0;
 
-    function playRound() {
+    let computerChoice = choices[Math.floor(Math.random() * 3)];
 
-        let computerChoice = choices[Math.floor(Math.random() * 3)];
-        console.log(computerChoice);
-    
-        if (playerChoice === "rock" && computerChoice === "scissors" ||
-            playerChoice === "paper" && computerChoice === "rock" ||
-            playerChoice === "scissors" && computerChoice === "paper") {
-            humanScore++;
-        } else if (playerChoice === computerChoice) {
-        } else {
-            computerScore++;
-        }
+    if (playerChoice === "rock" && computerChoice === "scissors" ||
+        playerChoice === "paper" && computerChoice === "rock" ||
+        playerChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+
+    } else if (playerChoice === computerChoice) {
+    } else {
+        computerScore++;
     }
-
-    // for (let i = 0; i < 5; i++) {
-        playRound();
-    // }
+    playerDisplay.textContent = `You chose: ${playerChoice}`;
+    computerDisplay.textContent = `Computer chooses: ${computerChoice}`;
+    roundDisplay.textContent = `Player: ${humanScore} - Computer: ${computerScore}`;
 }
 
-playGame();
+// for (let i = 0; i < 5; i++) {
+    playGame();
+// }
+
+
+
 
